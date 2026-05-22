35127a618b9145f4f71bb75b0941c333656dd8d4
taylor <taylor@hadron.lan>
2026-05-22 13:18:40 -0600

    commit

--- src/fetcher.ts
+++ src/fetcher.ts
@@ -1,14 +1,26 @@
-const CARTHAGE_API_URL = import.meta.env.VITE_CARTHAGE_API_URL;
+const getCarthageApiUrl = (): string => {
+  if (import.meta.env.DEV) {
+    const CARTHAGE_API_URL = import.meta.env.VITE_CARTHAGE_API_URL;
 
-if (!CARTHAGE_API_URL) {
-  throw new Error("'VITE_CARTHAGE_API_URL' not found in .env!");
-}
+    if (!CARTHAGE_API_URL) {
+      throw new Error("'VITE_CARTHAGE_API_URL' not found in .env!");
+    }
+
+    return `http://${CARTHAGE_API_URL}`;
+  }
+
+  const { protocol, hostname, port } = window.location;
+  const portSuffix = port ? `:${port}` : "";
+  return `${protocol}//${hostname}${portSuffix}/api/v1`;
+};
+
+const CARTHAGE_API_URL = getCarthageApiUrl();
 
 export const carthageFetcher = async <T>(
   endpointUrl: string,
   options?: RequestInit,
 ): Promise<T> => {
-  const response = await fetch(`http://${CARTHAGE_API_URL}${endpointUrl}`, {
+  const response = await fetch(`${CARTHAGE_API_URL}${endpointUrl}`, {
     headers: {
       "Content-Type": "application/json",
       ...options?.headers,
@@ -28,7 +40,7 @@ export const carthageFetcherUpload = async <T>(
   endpointUrl: string,
   options?: RequestInit,
 ): Promise<T> => {
-  const response = await fetch(`http://${CARTHAGE_API_URL}${endpointUrl}`, {
+  const response = await fetch(`${CARTHAGE_API_URL}${endpointUrl}`, {
     headers: {
       ...options?.headers,
     },
