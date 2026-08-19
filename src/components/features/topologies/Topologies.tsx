a2d2fa4742b14d6205ec2977f827018a94586dc1
taylor <taylor@hadron.lan>
2026-08-19 15:18:35 -0600

    added more labels, reset and CSS touchups

--- src/components/features/topologies/Topologies.tsx
+++ src/components/features/topologies/Topologies.tsx
@@ -1,12 +1,19 @@
 import { useQuery } from "@tanstack/react-query";
-import { Check, Equal, TriangleAlert, User } from "lucide-react";
-import { useState } from "react";
+import { Check, Equal, SlashIcon, TriangleAlert, User } from "lucide-react";
+import React, { useState } from "react";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { getDeploymentStatus, startDeploy } from "./hooks";
 import type { TopologyStatusResponseStatus } from "./types";
 
-type StatusFilter = {
+interface DeploymentStatusI {
   value: Exclude<keyof TopologyStatusResponseStatus, "running">;
   name: string;
   header: string;
@@ -15,9 +22,9 @@ type StatusFilter = {
   borderColor: string;
   textColor: string;
   iconElement: typeof Check;
-};
+}
 
-const STATUSES: StatusFilter[] = [
+const STATUSES: DeploymentStatusI[] = [
   {
     value: "successes",
     name: "Successes",
@@ -49,16 +56,6 @@ const STATUSES: StatusFilter[] = [
     iconElement: TriangleAlert,
   },
   {
-    value: "ignored",
-    name: "Ignored",
-    header: "IGNORED",
-    description: "",
-    bgColor: "bg-neutral-200",
-    borderColor: "border-neutral-500",
-    textColor: "text-neutral-900",
-    iconElement: Equal,
-  },
-  {
     value: "orphans",
     name: "Orphans",
     header: "ORPHAN",
@@ -68,31 +65,54 @@ const STATUSES: StatusFilter[] = [
     textColor: "text-purple-900",
     iconElement: User,
   },
+  {
+    value: "ignored",
+    name: "Ignored",
+    header: "IGNORED",
+    description: "",
+    bgColor: "bg-neutral-200",
+    borderColor: "border-neutral-500",
+    textColor: "text-neutral-900",
+    iconElement: Equal,
+  },
 ];
 
 export const DeployTopologyContainer = () => {
   const [optimisticDeployed, setOptimisticDeployed] = useState(false);
-  const [filters, setFilters] = useState([true, true, true, true, true]);
+  const [filters, setFilters] = useState(Array(STATUSES.length).fill(true));
 
   const { data: deploymentStatus } = useQuery({
     queryKey: [],
     queryFn: getDeploymentStatus,
-    refetchInterval: 1000,
+    refetchInterval: 2000,
   });
 
-  const { refetch } = useQuery({
+  const { refetch: startDeploymentRefetch } = useQuery({
     queryKey: [],
     queryFn: startDeploy,
     enabled: false,
   });
 
-  const startDeployment = () => {
+  const handleStartDeployment = () => {
     setOptimisticDeployed(true);
-    refetch();
+    startDeploymentRefetch();
   };
 
-  const stopDeployment = () => {
+  const handleStopDeployment = () => {
     setOptimisticDeployed(false);
+  };
+
+  const onDeploymentClick = () => {
+    if (isRunning) {
+      handleStopDeployment();
+      return;
+    }
+
+    handleStartDeployment();
+  };
+
+  const onResetFiltersClick = () => {
+    setFilters(Array(STATUSES.length).fill(true));
   };
 
   const handleFilterChange = (filterIndex: number) => {
@@ -103,21 +123,44 @@ export const DeployTopologyContainer = () => {
 
   const isRunning = deploymentStatus?.running ?? optimisticDeployed;
   const isRunningMessage = isRunning ? "Running" : "Not Running";
+  const messageCount = STATUSES.reduce((total, s) => {
+    return total + (deploymentStatus?.[s.value].length ?? 0);
+  }, 0);
 
   return (
-    <div>
-      <button
-        type="button"
-        onClick={() => (isRunning ? stopDeployment() : startDeployment())}
-        className={cn(
-          "mb-4 text-lg text-white font-semibold px-4 py-2 hover:opacity-90 hover:shadow-md cursor-pointer",
-          isRunning ? "bg-red-700" : "bg-blue-800",
-        )}
-      >
-        {isRunning ? "Stop" : "Start"} Deployment
-      </button>
+    <div className="flex flex-col gap-3">
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/deploy">All Topologies</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator>
+            <SlashIcon />
+          </BreadcrumbSeparator>
+          {/* <BreadcrumbItem>
+            <BreadcrumbPage>{topologyId}</BreadcrumbPage>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator>
+            <SlashIcon />
+          </BreadcrumbSeparator> */}
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/deploy">Deploy</BreadcrumbLink>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
 
-      <div className="flex flex-col gap-3">
+      <div className="flex gap-4 items-center">
+        <button
+          type="button"
+          onClick={onDeploymentClick}
+          className={cn(
+            "self-center text-lg text-white font-semibold px-4 py-2 hover:opacity-80 hover:shadow-md cursor-pointer w-48",
+            isRunning ? "bg-red-700" : "bg-blue-800",
+          )}
+        >
+          {isRunning ? "Stop" : "Start"} Deployment
+        </button>
+
         <div className="text-lg gap-2">
           <div className="flex gap-2 font-bold">
             Status:
@@ -128,55 +171,66 @@ export const DeployTopologyContainer = () => {
             </span>
           </div>
         </div>
+      </div>
 
-        <div className="flex gap-2 font-bold">
-          <span className="text-lg self-center">Filters:</span>
-          {STATUSES.map((statusType, i) => (
-            <Button
-              key={i}
-              className={cn(
-                "text-md hover:opacity-80 hover:shadow-md cursor-pointer",
-                statusType.bgColor,
-                statusType.borderColor,
-                statusType.textColor,
-                !filters[i] && "opacity-50",
-              )}
-              onClick={() => handleFilterChange(i)}
-            >
-              {statusType.name} (
-              {deploymentStatus?.[statusType.value].length ?? 0})
-            </Button>
-          ))}
-        </div>
+      <div className="flex gap-2 font-bold">
+        <span className="text-lg self-center">Message Filters:</span>
+        {STATUSES.map((statusType, i) => (
+          <Button
+            key={i}
+            className={cn(
+              "text-md hover:opacity-80 hover:shadow-md cursor-pointer",
+              statusType.bgColor,
+              statusType.borderColor,
+              statusType.textColor,
+              !filters[i] && "opacity-50",
+            )}
+            onClick={() => handleFilterChange(i)}
+          >
+            {statusType.name} (
+            {deploymentStatus?.[statusType.value].length ?? 0})
+          </Button>
+        ))}
+        <Button
+          type="button"
+          className="cursor-pointer"
+          variant="ghost"
+          onClick={onResetFiltersClick}
+        >
+          Reset
+        </Button>
+      </div>
 
-        <div className="flex flex-col gap-1 border-2">
-          {STATUSES.map((statusType, si) => (
-            <>
-              {filters[si] &&
-                deploymentStatus?.[statusType.value].map((message, mi) => (
+      <div className="border-1" />
+
+      <div className="text-lg font-bold">{messageCount} Messages:</div>
+      <div className="flex flex-col gap-1">
+        {STATUSES.map((statusType, si) => (
+          <React.Fragment key={si}>
+            {filters[si] &&
+              deploymentStatus?.[statusType.value].map((message, mi) => (
+                <div
+                  key={mi}
+                  className={cn(
+                    "px-4 py-2 border-l-6",
+                    statusType.bgColor,
+                    statusType.borderColor,
+                  )}
+                >
                   <div
-                    key={mi}
                     className={cn(
-                      "px-4 py-2 border-l-6",
-                      statusType.bgColor,
-                      statusType.borderColor,
+                      "flex self-end gap-2 font-semibold pb-1",
+                      statusType.textColor,
                     )}
                   >
-                    <div
-                      className={cn(
-                        "flex self-end gap-2 font-semibold pb-1",
-                        statusType.textColor,
-                      )}
-                    >
-                      <statusType.iconElement />
-                      {statusType.header}
-                    </div>
-                    {message}
+                    <statusType.iconElement />
+                    {statusType.header}
                   </div>
-                ))}
-            </>
-          ))}
-        </div>
+                  {message}
+                </div>
+              ))}
+          </React.Fragment>
+        ))}
       </div>
     </div>
   );
